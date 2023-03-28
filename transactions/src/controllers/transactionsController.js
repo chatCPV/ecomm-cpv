12ed25de93e9ed5da0e5641b120545bca215bf16
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-03-28 17:23:47 -0300

    altera chave no Transaction.js e termina implementacao da funcao createTransaction

--- transactions/src/controllers/transactionsController.js
+++ transactions/src/controllers/transactionsController.js
@@ -1,17 +1,30 @@
 const user = require('../helpers/mockUser.js');
 const Transactions = require('../models/Transaction.js');
 
-function createTransaction(req, res) {
+async function createTransaction(req, res) {
   try { 
     const transaction = req.body;
-    const { tansactionValue, cardHolder, cardNumber, cardExp, cardCvv } = transaction;
-    const { card } = user;
+    const { transactionValue, cardHolder, cardNumber, cardExp, cardCvv } = transaction;
+    const { monthIncome, card } = user;
 
     if (cardHolder !== card.cardHolder || cardNumber !== card.cardNumber || cardExp !== card.cardExp || cardCvv !== card.cardCvv ) {
-      const transactionRejected = { ...transaction, status: 'Rejected'}
+      const transactionRejected = { ...transaction, status: 'Rejeitada'};
+      const newTransactionRejected = await Transactions.create(transactionRejected);
 
-      return res.status(200).json(transactionRejected)
+      return res.status(200).json(newTransactionRejected);
     }
+
+    if (transactionValue >= (monthIncome/2)) {
+      const transactionUnderReview = { ...transaction, status: 'Em análise' };
+      const newTransactionUnderReview = await Transactions.create(transactionUnderReview);
+
+      return res.status(200).json(newTransactionUnderReview);
+    }
+
+    const approvedTransaction = { ...transaction, status: 'Aprovada' }
+    const newApprovedTransaction = await Transactions.create(approvedTransaction);
+
+    return res.status(200).json(newApprovedTransaction);
   } catch (error) { 
     return res.status(500).json(error.message); 
   }
