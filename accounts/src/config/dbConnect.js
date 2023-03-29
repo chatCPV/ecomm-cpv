0251bfe9fce1a298dcfbbcd7e29b666674275ef8
= <paulo.leite@pagonxt.com>
2023-03-28 21:44:06 -0300

    feat: criação do arquivo dbConnect.js

---
+++ accounts/src/config/dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const HOST = process.env.MONGO_ACCOUNT_HOST || "127.0.0.1";
+const USER = process.env.MONGO_ACCOUNT_USER || "admin";
+const PASSWORD = process.env.MONGO_ACCOUNT_PASSWORD || "secret";
+const DATABASE = process.env.MONGO_ACCOUNT_DATABASE || "ecomm-account";
+const PORT = process.env.MONGO_ACCOUNT_PORT || "27017";
+
+mongoose.connect(`mongodb://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}?authSource=admin`);
+
+const db = mongoose.connection;
+
+module.exports = db;
