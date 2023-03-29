fa44a11f31547ca194e94c753a6a4255aa5e9083
Ash Trindade <ash.trindade@pagonxt.com>
2023-03-29 17:01:45 -0300

    update env

--- transactions/src/config/dbconnect.js
+++ transactions/src/config/dbconnect.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
-// const path = require('path')
-// require('dotenv').config({ path: path.resolve(__dirname, './.env') })
-const URL = process.env.MONGO_TRANSACTIONS_URL
 
-mongoose.connect(URL)
+mongoose.connect(process.env.MONGO_DATABASE)
 
 const db = mongoose.connection
 
