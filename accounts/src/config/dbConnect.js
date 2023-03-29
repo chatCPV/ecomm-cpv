d09e86b12d1f410bfb902f007db0c44d6a4576bf
Rhian Moura <rhian.moura@pagonxt.com>
2023-03-29 12:07:45 -0300

    Fixing responses and docker-compose

--- accounts/src/config/dbConnect.js
+++ accounts/src/config/dbConnect.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
-const HOST = process.env.MONGO_ACCOUNT_HOST;
-const USER = process.env.MONGO_ACCOUNT_USER;
-const PASSWORD = process.env.MONGO_ACCOUNT_PASSWORD;
-const DATABASE = process.env.MONGO_ACCOUNT_DATABASE;
-const PORT = process.env.MONGO_ACCOUNT_PORT;
+const URL = process.env.MONGO_ACCOUNT_URL;
 
-mongoose.connect(`mongodb://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}?authSource=admin`);
+mongoose.connect(URL);
 
 const db = mongoose.connection;
 
