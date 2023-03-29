f049a9d1b7a0ee1933501284f23c7ccc1f4c71ec
= <paulo.leite@pagonxt.com>
2023-03-28 21:42:44 -0300

    feat: criação do arquivo server.js

---
+++ accounts/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const db = require('./src/config/dbConnect');
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 8001;
+
+db.on('error', console.log.bind(console, 'Erro de conexão!'));
+
+db.once('open', () => {
+  console.log('Conexão com o banto feita com sucesso');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escutando em http://localhost:${PORT}`);
+});
