203bdaecab5e45aecd2e19c22b6eb8c456889ec7
Rhian Moura <rhian.moura@pagonxt.com>
2023-03-30 15:06:09 -0300

    fetch api accounts

---
+++ anti-fraud/src/controllers/api.js
@@ -0,0 +1,7 @@
+async function useApiAccounts(id) {
+    const searchAccount = await fetch(`http://account-container:8001/accounts/${id}`);
+    const accountFound = await searchAccount.json();
+    return accountFound;
+  }
+  
+  module.exports = useApiAccounts;
